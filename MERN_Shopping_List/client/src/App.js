b8691fafaf2f5fd08300ad909d3a9b883a7d78f3
Cameron Shatto <45926493+CShatto99@users.noreply.github.com>
2020-07-10 18:16:31 -0500

    Added more redux
    Added an auth reducer and an error reducer

--- MERN_Shopping_List/client/src/App.js
+++ MERN_Shopping_List/client/src/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
-import { Provider } from 'react-redux';
-import { Container } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import AppNavbar from './components/AppNavbar';
 import ShoppingList from './components/ShoppingList';
 import ItemModal from './components/ItemModal';
+import { Container } from 'reactstrap';
+
+import { Provider } from 'react-redux';
 import store from './store';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import { loadUser } from './actions/authActions';
+
 
 
 
 class App extends Component {
+  componentDidMount() {
+    store.dispatch(loadUser());
+  }
+  
   render() {
     return (
       <Provider store={store}>
@@ -20,7 +26,6 @@ class App extends Component {
             <ItemModal />
             <ShoppingList />
           </Container>
-
         </div>
       </Provider>
     );
