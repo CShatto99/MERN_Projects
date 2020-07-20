34711dfdd75fb117d992dbd6419ea07179fc8c85
Cameron Shatto <45926493+CShatto99@users.noreply.github.com>
2020-07-20 17:06:00 -0500

    Created routes and User model

--- DevConnector/server.js
+++ DevConnector/server.js
@@ -1,10 +1,14 @@
 const express = require('express')
-
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('API running')
-})
+const connectDB = require('./config/db')
+
+connectDB()
+
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/posts', require('./routes/api/posts'))
 
 const PORT = process.env.PORT || 5000
 
