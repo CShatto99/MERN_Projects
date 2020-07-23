db3ddd3bca34b0aebd9a2dc5538bced0754e17db
Cameron Shatto <45926493+CShatto99@users.noreply.github.com>
2020-07-23 18:51:41 -0500

    Finished the alert redux

--- DevConnector/client/src/components/auth/Register.js
+++ DevConnector/client/src/components/auth/Register.js
@@ -1,7 +1,10 @@
 import React, { Fragment, useState } from 'react'
+import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { setAlert } from '../../actions/alert'
+import PropTypes from 'prop-types'
 
-const Register = () => {
+const Register = ({ setAlert }) => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -16,8 +19,10 @@ const Register = () => {
   const onSubmit = async e => {
     e.preventDefault()
 
-    if(password !== password2) console.log('Passwords do not match')
-    else console.log('SUCCESS')
+    if(password !== password2)
+      setAlert('Passwords do not match', 'danger')
+    else
+      console.log('SUCCESS')
   }
 
   return (
@@ -75,4 +80,8 @@ const Register = () => {
   )
 }
 
-export default Register
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired
+}
+
+export default connect(null, { setAlert })(Register)
