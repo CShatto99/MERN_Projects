df879af81a2aa8fd7bfaab96461ff1c30f8a61e1
Cameron Shatto <45926493+CShatto99@users.noreply.github.com>
2020-07-23 18:31:44 -0500

    Added landing, login, and sign up pages. Added alert redux

---
+++ DevConnector/client/src/actions/alert.js
@@ -0,0 +1,15 @@
+const { SET_ALERT, REMOVE_ALERT } from './types'
+import { v4 as uuid } from 'uuid'
+
+
+export const setAlert = (msg, alertType) => dispatch => {
+  const id = uuid.v4()
+  dispatch({
+    type: SET_ALERT,
+    payload: { msg, alertType, id }
+  })
+}
+
+export const removeAlert = () => dispatch => {
+
+}
