02522baf6c776379e7d5b9ba53ef438ca21d7003
Cameron Shatto <45926493+CShatto99@users.noreply.github.com>
2020-08-04 16:48:20 -0500

    npm init and created initial react app

---
+++ GeoLogger/client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MySketch from './components/MySketch'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <MySketch />
+    </div>
+  );
+}
+
+export default App;
---
+++ GeoLogger/server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+
+app.use('/api/test', require('./routes/api/test'))
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => console.log(`App listening on port ${port}`))
