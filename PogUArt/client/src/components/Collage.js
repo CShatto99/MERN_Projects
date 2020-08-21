a21e0e4c5d282e1f5d7fc6e6cd5e3cdb11213b12
Cameron Shatto <45926493+CShatto99@users.noreply.github.com>
2020-08-20 19:29:08 -0500

    Small changes to json file

--- PogUArt/client/src/components/Collage.js
+++ PogUArt/client/src/components/Collage.js
@@ -9,21 +9,23 @@ import images from '../json/images.json'
 const Collage = () => {
   const { art } = images
 
+  console.log(art[0])
+
   const collage = []
-  
+
   for(let i = 0; i < art.length; i+=2) {
-    if(i + 2 <art.length) {
+    if(i + 2 <= art.length) {
       collage.push(
-        <Row key={art[i].title} className='align-items-center mb-5'>
-          <Col><img src={require(`../xqcow/${art[i].title}`)} alt="PogU Art" /></Col>
-          <Col><img src={require(`../xqcow/${art[i+1].title}`)} alt="PogU Art" /></Col>
+        <Row key={art[i]} className='align-items-center mb-5'>
+          <Col><img src={require(`../xqcow/${art[i]}`)} alt="PogU Art" /></Col>
+          <Col><img src={require(`../xqcow/${art[i+1]}`)} alt="PogU Art" /></Col>
         </Row>
       )
     }
     else {
       collage.push(
         <Row key={art[i].title} className='align-items-center mb-5'>
-          <Col><img src={require(`../xqcow/${art[i].title}`)} alt="PogU Art" /></Col>
+          <Col><img src={require(`../xqcow/${art[i]}`)} alt="PogU Art" /></Col>
           <Col></Col>
         </Row>
       )
