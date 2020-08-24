71b613508daf8f633127fc4415ea763b4d522133
Cameron Shatto <45926493+CShatto99@users.noreply.github.com>
2020-08-24 13:40:48 -0500

    added more art

--- PogUArt/client/src/components/Collage.js
+++ PogUArt/client/src/components/Collage.js
@@ -6,77 +6,80 @@ import {
   Button
 } from 'reactstrap'
 import { v4 as uuidv4 } from 'uuid';
-import images from '../json/images.json'
+import { art } from '../json/images.json'
 import authors from '../json/authors.json'
 
-const Collage = () => {
-  const [state, setState] = useState({
-    collage: []
-  })
+const shuffle = () => {
+  let array = []
+  art.map((item, index) => array.push(index))
 
-  useEffect(() => {
-    onClickHandler()
-  }, [])
+  for(let i = art.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * i)
+    const temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
+  }
+  return array
+}
 
-  const { art } = images
-
-  const loadMore = index => {
-    let collage = []
-    let i = index
-    while(i < index+20) {
-      if(i + 2 <= art.length) {
-        collage.push(
-          <Fragment key={uuidv4()}>
-            <Row className='not-mobile align-items-center mb-5'>
-              <Col className='mr-5'>
-                <img src={art[i].url} alt='PogU Art' />
-              </Col>
-              <Col>
-                <img src={art[i+1].url} alt='PogU Art' />
-              </Col>
-            </Row>
-            <Row className='mobile-div align-items-center mb-5'>
-              <Col>
-                <img className='mb-5 ' src={art[i].url} alt='PogU Art' />
-                <img src={art[i+1].url} alt='PogU Art' />
-              </Col>
-            </Row>
-          </Fragment>
-        )
-        i+=2
-      }
-      else {
-        collage.push(
-          <Fragment>
-            <Row className='not-mobile align-items-center mb-5'>
-              <Col className='mobile-margin'>
-                <img src={art[i].url} alt='PogU Art' />
-              </Col>
-              <Col></Col>
-            </Row>
-          </Fragment>
-        )
-        i++
-        break
-      }
+const loadMore = (index, randex) => {
+  let collage = []
+  let i = index
+  while(i < index+20) {
+    if(i + 2 <= art.length) {
+      collage.push(
+        <Fragment key={uuidv4()}>
+          <Row className='not-mobile align-items-center mb-5'>
+            <Col className='mr-5'>
+              <img src={art[randex[i]].url} alt='PogU Art' />
+            </Col>
+            <Col>
+              <img src={art[randex[i+1]].url} alt='PogU Art' />
+            </Col>
+          </Row>
+          <Row className='mobile-div align-items-center mb-5'>
+            <Col>
+              <img className='mb-5 ' src={art[randex[i]].url} alt='PogU Art' />
+              <img src={art[randex[i+1]].url} alt='PogU Art' />
+            </Col>
+          </Row>
+        </Fragment>
+      )
+      i+=2
     }
-    return collage
+    else {
+      collage.push(
+        <Fragment>
+          <Row className='not-mobile align-items-center mb-5'>
+            <Col className='mobile-margin'>
+              <img src={art[randex[i]].url} alt='PogU Art' />
+            </Col>
+            <Col></Col>
+          </Row>
+        </Fragment>
+      )
+      i++
+      break
+    }
   }
+  return collage
+}
 
-  const onClickHandler = () => {
-    setState({
-      ...state,
-      collage: state.collage.concat(loadMore(state.collage.length*2))
-    })
-  }
+const Collage = () => {
+  const [randex, setRandex] = useState(shuffle())
+  const [collage, setCollage] = useState(loadMore(0, randex))
 
   return (
     <Fragment>
       <Container className='text-center'>
         <h1 className='mobile-header1'>PogU Art</h1>
         <h4 className='mobile-header4 mb-5'>{art.length} pieces by {authors.authors.length} authors</h4>
-        {state.collage.length > 0 && state.collage}
-        {state.collage.length*2 < art.length && <Button className='mb-5' size='sm' color='primary' onClick={() => onClickHandler()}>Load More</Button>}
+        {collage.length > 0 && collage}
+        {collage.length*2 < art.length &&
+          <Button className='mb-5' size='sm' color='primary' onClick={() => setCollage(collage.concat(loadMore(collage.length*2, randex)))}>
+            Load More
+          </Button>
+        }
       </Container>
     </Fragment>
   )
